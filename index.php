026a2249cb6ba8f0dedcc9449b9c05bd7ad2eaf5
Romulo Sousa <romulosousa27@hotmail.com>
2018-06-11 15:44:28 -0300

    form login, response fail or success

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
-<?php include("template/header.php"); ?>
+<?php include("template/header.php");?>
+<?php
+    // mensagem de erro ou sucesso para o login
+    if (isset($_GET['login']) && $_GET['login'] == true){
+        echo "<p class='alert-success'>Logado com Sucesso</p>";
+    }
+    elseif (isset($_GET['login']) && $_GET['login'] == false){
+        echo "<p class='alert-danger'>Usuário/Senha inválida</p>";
+    }
+?>
+
     <h1>Bem vindo!</h1>
+
+    <h2>Login</h2>    
+    <form action="login.php" method="post">
+        <table class="table">
+            <tr>
+                <td>Email</td>
+                <td><input class="form-control" type="email" name="email"></td>
+            </tr>
+            <tr>
+                <td>Senha</td>
+                <td><input class="form-control" type="password" name="senha"></td>
+            </tr>
+            <tr>
+                <td><button class="btn btn-primary">Login</button></td>
+            </tr>
+        </table>
+    </form>
 <?php include("template/footer.php"); ?>
